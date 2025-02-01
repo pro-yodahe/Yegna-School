150f140377fb4ef5cf6c67e580a369f6eb6671eb
pro-yodahe <yodahejob@gmail.com>
2025-02-01 22:27:26 +0300

    Updated project with new changes

---
+++ yegnaschool/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,64 @@
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import { IconBadge } from "@/components/icon-badge"
+import { LayoutDashboard } from "lucide-react"
+
+const CourseIdPage = async ({
+    params
+}: {
+    params: { courseId: string }
+}) => {
+    const { userId } = await auth()
+    if(!userId){
+        return redirect("/")
+    }   
+
+    const course = await db.course.findUnique({
+        where: {
+            id: params.courseId
+        }
+    });
+    if(!course){
+        return redirect("/")
+    }
+
+    const requredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId
+    ];
+
+    const totalFields = requredFields.length;
+    const compltedFilds = requredFields.filter(Boolean).length;
+
+    const completionText = `(${compltedFilds}/${totalFields})`
+
+    return ( 
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">
+                        Course setup
+                    </h1>
+                    <span className="text-sm text-slate-700">
+                        Complete all field {completionText}
+                    </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div className="flex items-center gap-x-2">
+                    <IconBadge size="sm" variant="success" icon={LayoutDashboard}/>
+                 <h2 className="text-xl ">
+                        Customize your course
+                    </h2>
+
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default CourseIdPage;
