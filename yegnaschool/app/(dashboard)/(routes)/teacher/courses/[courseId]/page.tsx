4bf0cb4bebfd2a38597715084707e34309c37043
pro-yodahe <yodahejob@gmail.com>
2025-02-02 13:38:13 +0300

    build Title wiith functionality but 404 Error

--- yegnaschool/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ yegnaschool/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 import { IconBadge } from "@/components/icon-badge"
 import { LayoutDashboard } from "lucide-react"
+import { TitleForm } from "./_components/title-form"
 
 const CourseIdPage = async ({
     params
@@ -39,7 +40,7 @@ const CourseIdPage = async ({
     return ( 
         <div className="p-6">
             <div className="flex items-center justify-between">
-                <div className="flex items-center justify-between flex-col gap-y-2">
+                <div className="flex flex-col gap-y-2">
                     <h1 className="text-2xl font-medium">
                         Course setup
                     </h1>
@@ -49,12 +50,18 @@ const CourseIdPage = async ({
                 </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
                 <div className="flex items-center gap-x-2">
-                    <IconBadge size="sm" variant="success" icon={LayoutDashboard}/>
+                    <IconBadge icon={LayoutDashboard}/>
                  <h2 className="text-xl ">
                         Customize your course
                     </h2>
-
+                    
+                </div>
+                    <TitleForm
+                    initialData={course}
+                    courseId={course.id}
+                    />
                 </div>
             </div>
         </div>
